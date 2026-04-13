1a0015a032e7fc4da17e2006a4a3100e386a7a3a
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2026-04-13 12:36:13 +0700

    update chart

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,46 +1,11 @@
 import SwiftUI
-import GoogleSignIn
-import FirebaseCore
 
 @main
 struct iOSApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-   var body: some Scene {
-      WindowGroup {
-            ContentView().onOpenURL(perform: { url in
-                GIDSignIn.sharedInstance.handle(url)
-            })
-      }
-   }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(
-      _ app: UIApplication,
-      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-    ) -> Bool {
-        FirebaseApp.configure()
-      return true
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-
-    func application(
-      _ app: UIApplication,
-      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-      var handled: Bool
-
-      handled = GIDSignIn.sharedInstance.handle(url)
-      if handled {
-        return true
-      }
-
-      // Handle other custom URL types.
-
-      // If not handled by this app, return false.
-      return false
-    }
-
-
 }
